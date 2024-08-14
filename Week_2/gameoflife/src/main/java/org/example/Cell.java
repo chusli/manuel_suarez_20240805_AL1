d308c286277c3ffe4f0ef1c00f3c4501f3d6a59c
Manuel <mantel1337@gmail.com>
2024-08-14 20:51:11 +0200

    reset for day 3 week 2

--- Week_2/gameoflife/src/main/java/org/example/Cell.java
+++
@@ -1,5 +0,0 @@
-package org.example;
-
-public record Cell(Coordinate coordinate, boolean alive) {
-
-}
--- Week_2/gameoflife/src/main/java/org/example/Coordinate.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public record Coordinate(int x, int y) {
-
-    public boolean neighbours(Coordinate equalCoodriante) {
-        return false;
-    }
-
-}
